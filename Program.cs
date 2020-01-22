79b0183ee8b42c48cda1b2cc34962b8e400d667b
AlonsoMejiaA <60183398+AlonsoMejiaA@users.noreply.github.com>
2020-01-22 09:33:06 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 
-namespace caso_2_tarea
+namespace caso_3_tarea
 {
     class Program
     {
         static void Main(string[] args)
         {
-             double Hipotenusa = 3.61;
-             double AnguloA = 33.69;
-             double AnguloRecto = 90.0;
-             double CatetoA = Hipotenusa * Math.Sin(AnguloA);
-             double AnguloC = (180.0 - AnguloRecto - AnguloA);
-             double CatetoB = Math.Sqrt((Hipotenusa * Hipotenusa) - (CatetoA * CatetoA));
+            double AnguloRecto = 90.0;
+            double AnguloC = 56.31;
+            double CatetoB = 3.0;
+            double CatetoA = CatetoB / Math.Tan(AnguloC);
+            double Hipotenusa = Math.Sqrt((CatetoA * CatetoA) + (CatetoB * CatetoB));
+            double AnguloA = Math.Atan(CatetoA / CatetoB);
             Console.Write("El cateto A ES: " + CatetoA + " El cateto B es: " + CatetoB + " La hipotenusa es: " + Hipotenusa + "los angulos son: " + AnguloRecto + " " + AnguloA + " " + AnguloC);
-
-
         }
     }
 }
