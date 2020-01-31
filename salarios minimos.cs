97fb3ad76297b14b337e6368bd065eac4a90f093
AlonsoMejiaA <60183398+AlonsoMejiaA@users.noreply.github.com>
2020-01-30 19:48:26 -0500

    Add files via upload

---
+++ salarios minimos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar su salario
+            Console.WriteLine("ingrese su salario");
+            double salarioDelSujeto = double.Parse(Console.ReadLine());
+
+            //Valor del SMMLV
+
+            double SMMLV = 877803.00;
+
+            //CONDICIONALES
+
+            if (salarioDelSujeto < (2 * SMMLV))
+            {
+                double ValorTarifa1 = (salarioDelSujeto * 2) / 100;
+                Console.WriteLine("su valor a pagar con su tarifa es: " + ValorTarifa1);
+            }
+            else if (salarioDelSujeto >= (2*SMMLV) && salarioDelSujeto < (4*SMMLV)) {
+
+                double ValorTarifa1 = (salarioDelSujeto * 5) / 100;
+                Console.WriteLine("su valor a pagar con su tarifa es: " + ValorTarifa1);
+            }
+            else{
+                double ValorTarifa1 = (salarioDelSujeto * 10) / 100;
+                Console.WriteLine("su valor a pagar con su tarifa es: " + ValorTarifa1);
+            }
+
+        }
+    }
+}
---
+++ tarifa y couta moderadora eps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar su salario
+            Console.WriteLine("ingrese su salario diario");
+            double salarioDelSujeto = double.Parse(Console.ReadLine());
+
+            //Valor del SMMLV
+
+            double SMLDV = 29260.10;
+
+            //CONDICIONALES
+
+            if (salarioDelSujeto < (2 * SMLDV))
+            {
+                double ValorTarifa1 = (salarioDelSujeto * 2) / 100;
+                double CoutaModeradora = (SMLDV * 11.7) / 100;
+                double total = ValorTarifa1 + CoutaModeradora;
+                Console.WriteLine("su valor a pagar con su tarifa es: " + ValorTarifa1 + " y el valor de su cuota moderadora es :" + CoutaModeradora + " para un total de: " + total);
+            }
+            else if (salarioDelSujeto >= (2 * SMLDV) && salarioDelSujeto < (5 * SMLDV))
+            {
+
+                double ValorTarifa1 = (salarioDelSujeto * 5) / 100;
+                double CoutaModeradora = (SMLDV * 46.1) / 100;
+                double total = ValorTarifa1 + CoutaModeradora;
+                Console.WriteLine("su valor a pagar con su tarifa es: " + ValorTarifa1 + " y el valor de su cuota moderadora es :" + CoutaModeradora + " para un total de: " + total);
+            }
+            else
+            {
+                double ValorTarifa1 = (salarioDelSujeto * 10) / 100;
+                double CoutaModeradora = (SMLDV * 121.5) / 100;
+                double total = ValorTarifa1 + CoutaModeradora;
+                Console.WriteLine("su valor a pagar con su tarifa es: " + ValorTarifa1 + " y el valor de su cuota moderadora es :" + CoutaModeradora + " para un total de: " + total);
+                
+            }
+        }
+    }
+}
